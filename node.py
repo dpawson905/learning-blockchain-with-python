a3ddc55f12d256979f28271ab9fae7b9ce058a51
Darrell Pawson <dpawson905@gmail.com>
2018-06-21 18:58:24 -0400

    fix references in node file

--- node.py
+++ node.py
@@ -21,7 +21,7 @@ class Node:
         return user_input
 
     def print_blockchain_elements(self):
-        for block in self.blockchain.chain:
+        for block in self.blockchain.get_chain():
             print('Outputting Block')
             print(block)
 
@@ -46,13 +46,13 @@ class Node:
                     print('Added transaction')
                 else:
                     print('Transaction failed')
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == '2':
                 self.blockchain.mine_block()
             elif user_choice == '3':
                 self.print_blockchain_elements()
             elif user_choice == '4':
-                if Verification.verify_transactions(self.blockchain.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print('All transactions are valid')
                 else:
                     print('There are invalid transactions')
@@ -62,7 +62,7 @@ class Node:
             else:
                 print('Input was invalid, please pick a value from the list!')
 
-            if not Verification.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.get_chain()):
                 self.print_blockchain_elements()
                 print('Invalid blockchain!')
                 break
